9c41f7c10e7241bc1ed571e11aefc22a714a741d
soxface <sonyacorcoran@gmail.com>
2015-02-11 17:57:09 +1100

    before adding email to database

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
     @user = User.new user_params
     if @user.save
       session[:user_id] = @user.id
-      redirect_to root_path
+      redirect_to users_path
     else
       render :new
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,6 @@ u3 = User.create(:name => 'Lynette', :password => 'chicken', :password_confirmat
 u4 = User.create(:name => 'Joel', :password => 'chicken', :password_confirmation => 'chicken', :remote_image_url => "http://chronicle.com/blogs/profhacker/files/2013/04/Professortocat_v2-300x300.png")
 admin = User.create(:name => 'admin', :password => 'admin', :password_confirmation => 'admin', :remote_image_url => "https://lh6.ggpht.com/Fudz3AikqUsxWYDgQHZMR1PSPCtyl1zVwJa1Do3B8roz3sOhmhIh6g_YHLVWqHo0EX4=w300", :is_admin => true)
 
-c1 = Cohort.create(:cohort => 'UG 1', :year => '2015')
+c1 = Cohort.create(:cohort => 'WDI 8', :year => '2015')
 
 c1.users << u1 << u2 << u3 << u4 << admin
